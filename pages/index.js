06a1b4b3de90610e157520ef41bb42f08f81e110
Pratigya <pratigyagg@gmail.com>
2022-07-13 12:20:14 +0530

    Title,logo

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,8 @@ export default function Home(){
     return(
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <Head>
-                <title>Create Next App</title>
-                <link rel="icon" href="/favicon.ico" />
+                <title>QuikShot</title>
+                <link rel="icon" href="https://raw.githubusercontent.com/Pratigya99/Images/main/instagram-small-logo.png" />
             </Head>
 
             <h1>This is the QUIKSHOT </h1>
