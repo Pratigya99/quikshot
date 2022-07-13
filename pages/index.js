4161a7dfa456193c758a8f007f35f3aa5756ac60
Pratigya <pratigyagg@gmail.com>
2022-07-13 12:22:02 +0530

    removed css

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 
 export default function Home(){
     return(
-        <div className="flex flex-col items-center justify-center min-h-screen py-2">
+        <div className="">
             <Head>
                 <title>QuikShot</title>
                 <link rel="icon" href="https://raw.githubusercontent.com/Pratigya99/Images/main/instagram-small-logo.png" />
