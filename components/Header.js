b8eea4d90710a4dad3ba1887f3b560d7babd362a
Pratigya <pratigyagg@gmail.com>
2022-07-13 15:07:39 +0530

    align instagram logos

--- components/Header.js
+++ components/Header.js
@@ -3,27 +3,29 @@ import Image from "next/image";
 function Header() {
     return (
         <div>
-            
+            <div className="flex justify-between max-w-6xl">
+                {/* Left */}
+                <div className="relative hidden lg:inline-grid h-24 w-24">
+        
+                    <Image src="https://raw.githubusercontent.com/Pratigya99/Images/main/instagram-text.png" layout="fill"
+                    objectFit="contain" />
+                </div>
+        
+                <div className="relative w-10 h-10 lg:hidden flex-shrink-0">
+                    <Image
+                     src="https://raw.githubusercontent.com/Pratigya99/Images/main/instagram-medium-logo.png"
+                     layout="fill"
+                     objectFit="contain"
+                    />
+                </div>
+        
+                {/* Middle */}
+        
+        
+                {/* Right */}
 
-            {/* Left */}
-            <div className="relation hidden lg:inline-grid h-24 w-24">
-
-                <Image src="https://raw.githubusercontent.com/Pratigya99/Images/main/instagram-text.png" layout="fill"
-                objectFit="contain" />
             </div>
 
-            <div className="relative w-10 h-10 lg:hidden flex-shrink-0">
-                <Image
-                 src="https://raw.githubusercontent.com/Pratigya99/Images/main/instagram-medium-logo.png"
-                layout="fill"
-                objectFit="contain"
-                />
-            </div>
-
-            {/* Middle */}
-
-
-            {/* Right */}
         </div>
     ) 
 }
