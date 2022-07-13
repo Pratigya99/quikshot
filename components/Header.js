034f188ec8df2592ba9bc3c3f27f7b667326676d
Pratigya <pratigyagg@gmail.com>
2022-07-13 14:29:25 +0530

    insert image instagram-text logo

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,14 @@
+import Image from "next/image";
+
 function Header() {
     return (
         <div>
             <h1>I am a Header</h1>
 
             {/* Left */}
-
+            <div>
+                <Image src="https://raw.githubusercontent.com/Pratigya99/Images/main/instagram-text.png" layout="fill" />
+            </div>
 
             {/* Middle */}
 
