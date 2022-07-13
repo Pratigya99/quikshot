1cacfb7764e7b9f7cf9fe08f8575fc9fca0c29d6
Pratigya <pratigyagg@gmail.com>
2022-07-13 15:36:22 +0530

    remove image height

--- components/Header.js
+++ components/Header.js
@@ -13,13 +13,13 @@ function Header() {
         <div>
             <div className="flex justify-between max-w-6xl">
                 {/* Left */}
-                <div className="relative hidden lg:inline-grid h-24 w-24 cursor-pointer">
+                <div className="relative hidden lg:inline-grid w-24 cursor-pointer">
         
                     <Image src="https://raw.githubusercontent.com/Pratigya99/Images/main/instagram-text.png" layout="fill"
                     objectFit="contain" />
                 </div>
         
-                <div className="relative w-10 h-10 lg:hidden flex-shrink-0 cursor-pointer">
+                <div className="relative w-10  lg:hidden flex-shrink-0 cursor-pointer">
                     <Image
                      src="https://raw.githubusercontent.com/Pratigya99/Images/main/instagram-medium-logo.png"
                      layout="fill"
