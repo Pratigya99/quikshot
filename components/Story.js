55c5be33496a17d8af8c610eb42051c38f9a1d87
Pratigya <pratigyagg@gmail.com>
2022-07-14 15:10:46 +0530

    add profile in story

--- components/Story.js
+++ components/Story.js
@@ -1,11 +1,10 @@
-function story() {
+function Story({  img, username }) {
     return (
         <div>
-
-
-
+            <img src={img} alt="" />
+            <p>{username}</p>
         </div>
-    )
+    );
 }
 
-export default Story
+export default Story;
