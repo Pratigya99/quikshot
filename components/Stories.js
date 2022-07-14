669caaee41bb345ba457ed591caa93e85f410146
Pratigya <pratigyagg@gmail.com>
2022-07-14 15:24:37 +0530

    add story in stories

--- components/Stories.js
+++ components/Stories.js
@@ -1,18 +1,28 @@
 import { faker } from '@faker-js/faker';
 import { useEffect, useState } from "react";
+import Story from "./Story";
 
 function Stories() {
     const [suggestions, setSuggestions] = useState([]);
 
     useEffect(() => {
         const suggestions = [...Array(20)].map((_, i) => ({
-            ...faker.name.firstName(),
+            username: faker.name.firstName(),
+            avatar: faker.image.avatar(),
             id: i,
         }));
         setSuggestions(suggestions);
+        console.log(suggestions);
     }, []);
     return (
         <div>
+            {suggestions.map((profile) => (
+                <Story 
+                    key={profile.id}
+                    img={profile.avatar}
+                    username={profile.username}
+                />
+            ))}
 
         </div>
     )
