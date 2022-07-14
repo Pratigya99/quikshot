2ea64de5f3e721b7c8930658a4457c837f2c2746
Pratigya <pratigyagg@gmail.com>
2022-07-14 14:56:02 +0530

    store suggetions in state

--- components/Stories.js
+++ components/Stories.js
@@ -1,15 +1,15 @@
 import { faker } from '@faker-js/faker';
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function Stories() {
-    
+    const [suggestions, setSuggestions] = useState([]);
 
     useEffect(() => {
         const suggestions = [...Array(20)].map((_, i) => ({
             ...faker.name.firstName(),
             id: i,
         }));
-        console.log(suggestions);
+        setSuggestions(suggestions);
     }, []);
     return (
         <div>
