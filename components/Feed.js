ce4178e93a72581c50bd561329c1f4d4338de3b8
Pratigya <pratigyagg@gmail.com>
2022-07-14 12:20:22 +0530

    set main to column

--- components/Feed.js
+++ components/Feed.js
@@ -2,7 +2,7 @@ import Stories from "./Stories";
 
 function Feed() {
     return (
-        <main>
+        <main className="grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl max-auto">
 
             <section>
                 {/* Stories */}
