f7eb259eca91366b4437c2524225bda16635eccb
Pratigya <pratigyagg@gmail.com>
2022-07-14 12:22:35 +0530

    first section takes 2column

--- components/Feed.js
+++ components/Feed.js
@@ -4,7 +4,7 @@ function Feed() {
     return (
         <main className="grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl max-auto">
 
-            <section>
+            <section className="col-span-2">
                 {/* Stories */}
                 <Stories />
                 {/* Posts */}
