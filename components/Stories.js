d44b9a7fb083e55c581fe5de9b6d13ee7d6a1ab9
Pratigya <pratigyagg@gmail.com>
2022-07-14 14:40:14 +0530

    get fake name from faker.js

--- components/Stories.js
+++ components/Stories.js
@@ -1,6 +1,16 @@
 import { faker } from '@faker-js/faker';
+import { useEffect } from "react";
 
 function Stories() {
+    
+
+    useEffect(() => {
+        const suggestions = [...Array(20)].map((_, i) => ({
+            ...faker.name.firstName(),
+            id: i,
+        }));
+        console.log(suggestions);
+    }, []);
     return (
         <div>
 
