190c442b84485893f988b86d8aea6356dde6e5e1
Pratigya <pratigyagg@gmail.com>
2022-07-14 11:43:12 +0530

    chat notification

--- components/Header.js
+++ components/Header.js
@@ -48,6 +48,9 @@ function Header() {
                     <MenuIcon className="h-6 md:hidden cursor-pointer" />
                     <div className="relative navBtn">
                         <PaperAirplaneIcon className="navBtn rotate-45" />
+                        <div className="absolute -top-1 -right-2 text-xs w-5 h-5 bg-red-500 rounded-full flex items-center justify-center animate-pulse text-white">
+                            3
+                        </div>
                     </div>
                     <PlusCircleIcon className="navBtn" />
                     <UserGroupIcon className="navBtn" />
