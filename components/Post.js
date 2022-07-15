b41a7a3c5dd07b909f5eb9ff3a78e46efa00841e
Pratigya <pratigyagg@gmail.com>
2022-07-15 15:20:12 +0530

    Import Icons

--- components/Post.js
+++ components/Post.js
@@ -1,7 +1,17 @@
-function Post() {
+import {
+    BookmarkIcon,
+    ChatIcon,
+    DotsHorizonalIcon,
+    EmojiHappyIcon,
+    HeartIcon,
+    PaperAirplaneIcon,
+} from "@heroicons/react/outline";
+import { HeartIcon as HeartIconFilled } from "@heroicons/react/solid";
+
+function Post({ id, username, userImg, img, caption }) {
     return (
         <div>
-
+            
         </div>
     )
 }
