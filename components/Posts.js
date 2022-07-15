29fa1c0995f835dbaa1256379f9f08cef9f3fd21
Pratigya <pratigyagg@gmail.com>
2022-07-15 15:10:39 +0530

    Dummy Post

--- components/Posts.js
+++ components/Posts.js
@@ -1,9 +1,37 @@
 import Post from "./Post";
 
+const posts = [
+    {
+        id: "123",
+        username: "ssssangha",
+        userImg: "https://raw.githubusercontent.com/Pratigya99/Images/main/profile-photo.png",
+        img: "https://raw.githubusercontent.com/Pratigya99/Images/main/post1.jpg",
+        caption: "this is a dummy post1!",
+    },
+    {
+        id: "123",
+        username: "ssssangha",
+        userImg: "https://raw.githubusercontent.com/Pratigya99/Images/main/profile-photo.png",
+        img: "https://raw.githubusercontent.com/Pratigya99/Images/main/post2.jpg",
+        caption: "this is a dummy post2!",
+    },
+];
+
 function Posts() {
     return (
         <div>
-            <Post />
+            {posts.map((post)=>(
+                <Post
+                    key={post.id}
+                    id={post.id}
+                    username={post.username}
+                    userImg={post.userImg}
+                    img={post.img}
+                    caption={post.caption}
+                />
+
+            ))}
+            
         </div>
 
     )
