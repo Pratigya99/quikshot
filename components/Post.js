ab449e034cba31076cbd4927598134510a8c0c01
Pratigya <pratigyagg@gmail.com>
2022-07-15 15:32:28 +0530

    Post header

--- components/Post.js
+++ components/Post.js
@@ -1,7 +1,7 @@
 import {
     BookmarkIcon,
     ChatIcon,
-    DotsHorizonalIcon,
+    DotsHorizontalIcon,
     EmojiHappyIcon,
     HeartIcon,
     PaperAirplaneIcon,
@@ -11,7 +11,17 @@ import { HeartIcon as HeartIconFilled } from "@heroicons/react/solid";
 function Post({ id, username, userImg, img, caption }) {
     return (
         <div className="bg-white my-7 border rounded-sm">
-            
+            <div className ="flex items-center p-5">
+                <img
+                    src={userImg}
+                    className="rounded-full h-12 w-12 object-contain border p-1 mr-3"
+                    alt=""
+                />
+                <p className="flex-1 font-bold">
+                    {username}
+                </p>
+                <DotsHorizontalIcon className="h-5" />
+            </div>
         </div>
     )
 }
--- components/Posts.js
+++ components/Posts.js
@@ -3,14 +3,14 @@ import Post from "./Post";
 const posts = [
     {
         id: "123",
-        username: "ssssangha",
+        username: "Pratigya",
         userImg: "https://raw.githubusercontent.com/Pratigya99/Images/main/profile-photo.png",
         img: "https://raw.githubusercontent.com/Pratigya99/Images/main/post1.jpg",
         caption: "this is a dummy post1!",
     },
     {
         id: "123",
-        username: "ssssangha",
+        username: "Pratigya",
         userImg: "https://raw.githubusercontent.com/Pratigya99/Images/main/profile-photo.png",
         img: "https://raw.githubusercontent.com/Pratigya99/Images/main/post2.jpg",
         caption: "this is a dummy post2!",
