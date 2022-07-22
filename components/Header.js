1ea4f853db01cb0a424b7661af47d66ac2735074
Pratigya <pratigyagg@gmail.com>
2022-07-22 11:53:09 +0530

    login logout functionality

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { signIn, signOut, useSession } from "next-auth/react";
 import {
     SearchIcon,
     PlusCircleIcon,
@@ -10,6 +11,8 @@ import {
 import { HomeIcon } from "@heroicons/react/solid";
 
 function Header() {
+    const { data: session } = useSession();
+    
     return (
         <div className="shadow-sm border-b bg-white sticky top-0 z-50">
             <div className="flex justify-between max-w-6xl mx-5 lg:mx-auto">
@@ -46,21 +49,29 @@ function Header() {
                 <div className="flex items-center justify-end space-x-4">
                     <HomeIcon className="navBtn" />
                     <MenuIcon className="h-6 md:hidden cursor-pointer" />
-                    <div className="relative navBtn">
-                        <PaperAirplaneIcon className="navBtn rotate-45" />
-                        <div className="absolute -top-1 -right-2 text-xs w-5 h-5 bg-red-500 rounded-full flex items-center justify-center animate-pulse text-white">
-                            3
-                        </div>
-                    </div>
-                    <PlusCircleIcon className="navBtn" />
-                    <UserGroupIcon className="navBtn" />
-                    <HeartIcon className="navBtn" />
+                    {session ? (
+                        <>
 
-                    <img
-                     src="https://raw.githubusercontent.com/Pratigya99/Images/main/profile-photo.png"
-                     alt="profile-photo"
-                     className="h-10 rounded-full cursor-pointer"
-                     />
+                            <div className="relative navBtn">
+                                <PaperAirplaneIcon className="navBtn rotate-45" />
+                                <div className="absolute -top-1 -right-2 text-xs w-5 h-5 bg-red-500 rounded-full flex items-center justify-center animate-pulse text-white">
+                                    3
+                                </div>
+                            </div>
+                            <PlusCircleIcon className="navBtn" />
+                            <UserGroupIcon className="navBtn" />
+                            <HeartIcon className="navBtn" />
+        
+                            <img
+                             onClick={signOut}
+                             src={session.user.image}
+                             alt="profile-photo"
+                             className="h-10 w-10 rounded-full cursor-pointer"
+                             />
+                        </>
+                    ) : (
+                        <button onClick={signIn}>Sign In</button>
+                    )}
                 </div>
 
             </div>
