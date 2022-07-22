71caef35e6b9410b1efbb2d161d2c8040ef090f9
Pratigya <pratigyagg@gmail.com>
2022-07-22 13:20:10 +0530

    home router functionality

--- components/Header.js
+++ components/Header.js
@@ -9,13 +9,15 @@ import {
     MenuIcon,
 } from "@heroicons/react/outline";
 import { HomeIcon } from "@heroicons/react/solid";
+import { useRouter } from "next/router";
 
 function Header() {
     const { data: session } = useSession();
+    const router = useRouter();
     
     return (
         <div className="shadow-sm border-b bg-white sticky top-0 z-50">
-            <div className="flex justify-between max-w-6xl mx-5 lg:mx-auto">
+            <div onClick={() => router.push('/')} className="flex justify-between max-w-6xl mx-5 lg:mx-auto">
                 {/* Left */}
                 <div className="relative hidden lg:inline-grid w-24 cursor-pointer">
         
@@ -23,7 +25,7 @@ function Header() {
                     objectFit="contain" />
                 </div>
         
-                <div className="relative w-10  lg:hidden flex-shrink-0 cursor-pointer">
+                <div onClick={() => router.push('/')} className="relative w-10  lg:hidden flex-shrink-0 cursor-pointer">
                     <Image
                      src="https://raw.githubusercontent.com/Pratigya99/Images/main/instagram-medium-logo.png"
                      layout="fill"
@@ -47,7 +49,7 @@ function Header() {
         
                 {/* Right */}
                 <div className="flex items-center justify-end space-x-4">
-                    <HomeIcon className="navBtn" />
+                    <HomeIcon onClick={() => router.push('/')} className="navBtn" />
                     <MenuIcon className="h-6 md:hidden cursor-pointer" />
                     {session ? (
                         <>
